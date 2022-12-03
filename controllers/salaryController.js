5dcd83ecca622b40f36d37af9550bd4ae6877109
pascal029 <pascalvirgiawan05@gmail.com>
2022-12-03 14:52:28 +0700

    count salary routes API done

--- controllers/salaryController.js
+++ controllers/salaryController.js
@@ -1,6 +1,22 @@
+const Salary = require("../models/salary");
+
 class SalaryController {
-  static async hitunggaji(req, res) {
-    res.status(200).json("1.799.000");
+  static hitunggaji(req, res) {
+    const { employee, komponengaji } = req.body;
+    const input = {
+      statusPerkawinan: employee.simbol,
+      kewarganegaraan: employee.kewarganegaraan.toLowerCase(),
+      gajiBersih: komponengaji[0].gajiBersih,
+      asuransi: komponengaji.length > 1 ? komponengaji[1].asuransi : 0,
+    };
+
+    Salary.hitunggaji(input, (err, pajak) => {
+      if (err) {
+        res.status(500).json(err);
+      } else {
+        res.status(200).json(pajak);
+      }
+    });
   }
 }
 
